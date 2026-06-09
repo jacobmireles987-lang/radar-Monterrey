0d4c27295c4a35ee444310396996b5ab29da3837
jacobmireles987-lang <jacobmireles987@gmail.com>
2026-06-09 09:07:21 -0600

    Update fb_extractor.py

--- fb_extractor.py
+++ fb_extractor.py
@@ -1,12 +1,12 @@
 def obtener_posts_monterrey():
     return [
-        "Busco minisplit inverter Mirage 1 tonelada barato urge",
-        "Alguien vende llantas Michelin rin 15 usadas?",
-        "Busco celular Samsung S21, doy efectivo hoy",
-        "Compro laptop Dell para diseño gráfico",
-        "Dónde consigo minisplits Mirage a buen precio?",
-        "Busco llantas Pirelli rin 15",
-        "Compro laptop Apple buen estado",
-        "Busco minisplit LG de ventana",
-        "Alguien vende Samsung S22 usado?"
+        "Busco minisplit inverter Mirage 1 tonelada barato pago $4500",
+        "Alguien vende llantas Michelin rin 15 usadas? traigo $1200",
+        "Busco celular Samsung S21, doy $5500 en efectivo hoy",
+        "Compro laptop Dell para diseño gráfico presupuesto $8000",
+        "Dónde consigo minisplits Mirage a buen precio? Maximo $5000",
+        "Busco llantas Pirelli rin 15 pago $1500",
+        "Compro laptop Apple buen estado tengo $12000",
+        "Busco minisplit LG de ventana pago $3000",
+        "Alguien vende Samsung S22 usado? ofrezco $7000"
     ]
